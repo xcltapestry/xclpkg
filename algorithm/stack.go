82e5fda1e4bab0f350fa02128bb56af996831f3d
XiongChuanLiang <xcl_168@aliyun.com>
2015-01-30 14:44:52 +0800

    Create stack.go

---
+++ algorithm/stack.go
@@ -0,0 +1,89 @@
+package algorithm
+
+//Stack
+//author:Xiong Chuan Liang
+//date:2015-1-30
+
+
+import (	
+	"errors"
+	"fmt"
+)
+
+
+type Stack struct {
+	Element []interface{} //Element
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+
+func (stack *Stack)Push(Value ...interface{}){
+	stack.Element = append(stack.Element,Value...)	
+}
+
+
+//从Stack移除元素
+func (stack *Stack)Pop()(err error){
+	if stack.Size()> 0 {
+		stack.Element = stack.Element[:stack.Size() - 1]
+		return nil
+	}
+	return errors.New("Stack为空.") //read empty stack
+}
+
+//返回下一个元素
+func (stack *Stack)Top()(Value interface{}){
+	if stack.Size() > 0 {
+		return stack.Element[stack.Size() - 1]
+	}
+	return nil //read empty stack
+}
+
+//交换值
+func (stack *Stack)Swap(other *Stack){ 
+	switch{
+	case stack.Size() == 0 && other.Size() == 0:
+		return 		
+	case other.Size() == 0 :
+		other.Element = stack.Element[:stack.Size()]
+		stack.Element = nil
+	case stack.Size()== 0 :
+		stack.Element = other.Element
+		other.Element = nil
+	default:
+		stack.Element,other.Element = other.Element,stack.Element
+	}
+	return 	
+}
+
+//修改指定索引的值
+func (stack *Stack)Set(idx int,Value interface{})(err error){
+	if idx >= 0 && stack.Size() > 0 && stack.Size() > idx{
+		stack.Element[idx] = Value
+		return nil
+	}
+	return errors.New("Set失败!")
+}
+
+//Stack的size
+func (stack *Stack)Size()(int){
+	return len(stack.Element)
+}
+
+//是否为空
+func (stack *Stack)Empty()(bool){
+	if stack.Size() > 0 {
+		return true
+	}
+	return false
+}	
+
+//打印
+func (stack *Stack)Print(){
+	for i := len(stack.Element) - 1; i >= 0; i--{	
+		fmt.Println(i,"=>",stack.Element[i])
+	}
+}
